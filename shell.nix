ce72cfa2d25522baaf23c336f50a846a111a6de6
Adriano Barbosa <barbosa.aob@gmail.com>
2022-12-13 06:38:54 -0400

    Atualiza shell.nix

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
 pkgs.mkShell {
   buildInputs = [
     pkgs.gnumake
-    pkgs.python39Packages.markdown
-    pkgs.python39Packages.pelican
+    pkgs.python310Packages.markdown
+    pkgs.python310Packages.pelican
   ];
 }
